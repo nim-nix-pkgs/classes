6a26f26fbad53b49151bb9d8acc97e83a3c2b9e8
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:13 -0300

    chore: re-index ../nimpkgs/c/classes/v0_2_9

--- v0_2_9/flake.nix
+++ v0_2_9/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-classes-v0_2_9.flake = false;
   inputs.src-classes-v0_2_9.owner = "jjv360";
-  inputs.src-classes-v0_2_9.ref   = "refs/tags/v0.2.9";
+  inputs.src-classes-v0_2_9.ref   = "v0_2_9";
   inputs.src-classes-v0_2_9.repo  = "nim-classes";
   inputs.src-classes-v0_2_9.type  = "github";
   
